dc6454ba7e572d391c06bbfa2ae588aba22a7a6a
nutricionistaalmeidavh-spec <nutricionistaalmeidavh@gmail.com>
2026-09-20 12:31:46 -0300

    test: scaffold admin RBAC acceptance

---
+++ tests/admin-rbac-ui.test.js
@@ -0,0 +1,6 @@
+import test from 'node:test';
+import assert from 'node:assert/strict';
+
+test('admin RBAC surface is implemented',()=>{
+  assert.fail('administration UI not implemented yet');
+});
